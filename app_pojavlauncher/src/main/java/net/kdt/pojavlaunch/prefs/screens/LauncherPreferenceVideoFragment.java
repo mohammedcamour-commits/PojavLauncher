a3dd0a89f5dec71caf41a2e60e2ea1b56aefa12b
WOOD6563 <arparnasing@gmail.com>
2026-08-16 22:02:28 +0530

    add button

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceVideoFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceVideoFragment.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.SwitchPreference;
 import androidx.preference.SwitchPreferenceCompat;
 
@@ -89,5 +90,8 @@ public class LauncherPreferenceVideoFragment extends LauncherPreferenceFragment 
     private void computeVisibility(){
         requirePreference("force_vsync", SwitchPreferenceCompat.class)
                 .setVisible(LauncherPreferences.PREF_USE_ALTERNATE_SURFACE);
+       String currentRenderer = LauncherPreferences.DEFAULT_PREF.getString("renderer", "opengles2");
+       boolean isMobileGluesRenderer = "opengles_mobileglues".equals(currentRenderer);
+       requirePreference("renderer_settings", Preference.class).setVisible(isMobileGluesRenderer);
     }
 }
