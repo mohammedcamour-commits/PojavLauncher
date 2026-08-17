2da642219a14163869c777b8510bb7a01d077a04
WOOD6563 <arparnasing@gmail.com>
2026-08-17 19:45:55 +0530

    fix

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/LauncherPreferences.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/LauncherPreferences.java
@@ -82,10 +82,10 @@ public class LauncherPreferences {
     public static int PREF_MG_DEPTH_CLEAR_FIX = 0;
     public static int PREF_MG_GL_VERSION = 0;
     public static int PREF_MG_GLSL_CACHE_MIB = 32;
-    public static boolean PREF_MG_EXT_COMPUTE_SHADER = false;
-    public static boolean PREF_MG_EXT_TIMER_QUERY = true;
-    public static boolean PREF_MG_EXT_DIRECT_STATE_ACCESS = false;
-    public static boolean PREF_MG_FSR1 = false;
+    public static boolean PREF_MG_EXT_COMPUTE_SHADER;
+    public static boolean PREF_MG_EXT_TIMER_QUERY;
+    public static boolean PREF_MG_EXT_DIRECT_STATE_ACCESS;
+    public static boolean PREF_MG_FSR1;
 
     public static void loadPreferences(Context ctx) {
         //Required for CTRLDEF_FILE and MultiRT
@@ -134,15 +134,15 @@ public class LauncherPreferences {
         PREF_MIGRATION_NOTICE = DEFAULT_PREF.getBoolean("migrationNotice", true);
         PREF_ALSOFT_FORCE_OPENSL = DEFAULT_PREF.getBoolean("alsoftForceOpenSL", false);
 
-        PREF_MG_ANGLE = DEFAULT_PREF.getInt("enableANGLE", 1);
-        PREF_MG_NO_ERROR = DEFAULT_PREF.getInt("enableNoError", 0);
-        PREF_MG_DEPTH_CLEAR_FIX = DEFAULT_PREF.getInt("angleDepthClearFixMode", 0);
-        PREF_MG_GL_VERSION = DEFAULT_PREF.getInt("customGLVersion", 0);
+        PREF_MG_ANGLE = Integer.parseInt(DEFAULT_PREF.getString("enableANGLE", "1"));
+        PREF_MG_NO_ERROR = Integer.parseInt(DEFAULT_PREF.getString("enableNoError", "0"));
+        PREF_MG_DEPTH_CLEAR_FIX = Integer.parseInt(DEFAULT_PREF.getString("angleDepthClearFixMode", "0"));
+        PREF_MG_GL_VERSION = Integer.parseInt(DEFAULT_PREF.getString("customGLVersion", "0"));
         PREF_MG_GLSL_CACHE_MIB = DEFAULT_PREF.getInt("maxGlslCacheSize", 32);
-        PREF_MG_EXT_COMPUTE_SHADER = DEFAULT_PREF.getBoolean("enableExtComputeShader", false);
-        PREF_MG_EXT_TIMER_QUERY = DEFAULT_PREF.getBoolean("enableExtTimerQuery", true);
-        PREF_MG_EXT_DIRECT_STATE_ACCESS = DEFAULT_PREF.getBoolean("enableExtDirectStateAccess", false);
-        PREF_MG_FSR1 = DEFAULT_PREF.getBoolean("enableFsr1", false);
+        PREF_MG_EXT_COMPUTE_SHADER = DEFAULT_PREF.getBoolean("enableExtComputeShader", false) ? 1 : 0;
+        PREF_MG_EXT_TIMER_QUERY = DEFAULT_PREF.getBoolean("enableExtTimerQuery", false) ? 1 : 0;
+        PREF_MG_EXT_DIRECT_STATE_ACCESS = DEFAULT_PREF.getBoolean("enableExtDirectStateAccess", false) ? 1 : 0;
+        PREF_MG_FSR1 = DEFAULT_PREF.getBoolean("enableFsr1", false) ? 1 : 0;
 
         String argLwjglLibname = "-Dorg.lwjgl.opengl.libname=";
         for (String arg : JREUtils.parseJavaArguments(PREF_CUSTOM_JAVA_ARGS)) {
